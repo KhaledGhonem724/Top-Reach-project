a2ea80415bcce396717d7601c4d7bd8d0d649bcc
KhaledGhonem724 <KhaledGhonem724@gmail.com>
2023-09-17 18:24:57 +0300

    finish footer

--- index.php
+++ index.php
@@ -5,15 +5,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script src="sections/tailwind.js"></script>
+
 </head>
 
 <body>
     <?php
-    require "sections/navbar.html";
-    require "sections/first.html";
-    require "sections/services.html";
-    require "sections/aboutus.html";
-    require "sections/clients.html";
+    // require "sections/navbar.html";
+    // require "sections/first.html";
+    // require "sections/services.html";
+    // require "sections/aboutus.html";
+    // require "sections/clients.html";
     require "sections/footer.html";
     ?>
 </body>
