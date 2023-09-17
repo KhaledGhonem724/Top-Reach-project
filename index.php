465b925ed1cf152823e20b19e34940d1e2c46fba
KhaledGhonem724 <KhaledGhonem724@gmail.com>
2023-09-17 13:14:12 +0300

    project setup

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    require "sections/navbar.html";
+    require "sections/first.html";
+    require "sections/services.html";
+    require "sections/aboutus.html";
+    require "sections/clients.html";
+    require "sections/footer.html";
+    ?>
+</body>
+
+</html>
